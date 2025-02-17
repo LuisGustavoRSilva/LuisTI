af60c1e608171b2f8938734a5d0eddb02f9945dc
LuisGustavoRSilva <luisgustavo050705@gmail.com>
2025-02-17 17:17:23 -0300

    janela html,css e js

---
+++ main.js
@@ -0,0 +1,36 @@
+console.log("Processo principal")
+
+const { app, BrowserWindow, nativeTheme } = require('electron')
+
+// Janela principal
+let win
+const createWindow = () => {
+    // a linha abaixo define o tema claro ou escuro
+  nativeTheme.themeSource = "light" // dark = escuro | light = claro
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    //autoHideMenuBar: true,
+    //minimizable: false,
+    resizable: false
+  })
+
+  win.loadFile('./src/views/index.html')
+}
+
+// Iniciar a aplicação
+app.whenReady().then(() => {
+  createWindow()
+    
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
